bcd5716f231b1bb86eb89941b5a2b16493dac4d3
arpan <arpandas020498@gmail.com>
2023-06-05 18:17:30 +0530

    product created

--- src/environment.ts
+++ src/environment.ts
@@ -2,6 +2,7 @@
 export const environment = {
   Client_Id: "116735044274-9kgqu0s8tn9et2fannkcirfvfhof5m5d.apps.googleusercontent.com",
   Client_secret: "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",
-  Redirect_Uri:"http://localhost:3000/oauth2/callback"
+  Redirect_Uri:"http://localhost:3000/oauth2/callback",
+  MerchantId:'764314763'
 }
 console.log(environment.Client_Id)
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,16 @@
 // eslint-disable-next-line no-console
 
-import { google } from 'googleapis';
+import { google, content_v2 } from 'googleapis';
 import { OAuth2Client } from 'google-auth-library';
 import { environment } from './environment';
-
 
 const authClient = new OAuth2Client(environment.Client_Id, environment.Client_secret, environment.Redirect_Uri);
 
 const authUrl = authClient.generateAuthUrl({
-    access_type: 'offline',
-    scope: 'https://www.googleapis.com/auth/content',
-  });
-console.log(authUrl)  
+  access_type: 'offline',
+  scope: 'https://www.googleapis.com/auth/content',
+});
+console.log(authUrl)
 
 const tokensPromise = authClient.getToken("4/0AbUR2VMnZ9ffNaf3jVKp9aKyMcKAqiHmRo2cLbLhq7-msNKSF-JXZO6GK9mD-fkPjNVw8g")
   .then(tokens => {
@@ -19,7 +18,7 @@ const tokensPromise = authClient.getToken("4/0AbUR2VMnZ9ffNaf3jVKp9aKyMcKAqiHmRo
     return tokens.tokens;
   })
   .catch(error => {
-   
+
     console.error('Error fetching tokens:', error);
     throw error;
   });
@@ -27,12 +26,37 @@ const tokensPromise = authClient.getToken("4/0AbUR2VMnZ9ffNaf3jVKp9aKyMcKAqiHmRo
 
 tokensPromise
   .then(tokens => {
-    
+
     console.log('Tokens:', tokens);
+   createProduct();
   })
   .catch(error => {
-    
+
     console.error('Error handling tokens:', error);
   });
 
-  
+  async function createProduct(){
+    try{
+      const merchantId=environment.MerchantId;
+      const productData:content_v2.Schema$Product={
+        title: 'Laptop',
+      description: 'i9 processer, 2080 gtx',     
+      price: {
+        value: '10.99',
+        currency: 'USD',
+      },
+      availability: 'in stock',
+      condition: 'new',
+      };
+      const content= google.content({version:'v2', auth:authClient})
+      const response=await content.products.insert({
+        merchantId,
+        requestBody: productData,
+      });
+      console.log('Product created:',response.data);
+    }
+    catch (error){
+      console.error('Error creating product')
+    }
+  }
+
